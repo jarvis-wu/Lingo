77bff7907171f65e6160895fd2d77ec25aa83595
Jarvis Wu <z235wu@edu.uwaterloo.ca>
2019-07-15 17:26:07 -0700

    Print learned words

--- duo.py
+++ duo.py
@@ -3,34 +3,64 @@
 # Dependency: duolingo-api (https://github.com/KartikTalwar/Duolingo)
 
 import duolingo
+import sys
 
 lingo = None
+username = ""
+password = ""
 
 def welcome():
     print("Welcome to Lingo, the Duolingo CLI app.")
+    getCredentials()
+
+def getCredentials():
+    global username, password
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
     askLogin()
 
 def askLogin():
-    myUsername = input("Enter your username: ")
-    myPassword = input("Enter your password: ")
     yes = {'yes','y', 'ye', ''}
     no = {'no','n'}
     choice = input("Do you want to log in? [y/n]: ").lower()
     if choice in yes:
-       login(myUsername, myPassword)
+        login()
     elif choice in no:
-       welcome()
+        print("Login aborted.")
+        welcome()
     else:
-       print("Please respond with 'y' or 'n'")
+        print("Please respond with 'y' or 'n'")
+        askLogin()
 
-def login(username, password):
+def login():
     global lingo
     lingo = duolingo.Duolingo(username, password=password)
+    print("Login successful!")
     printUserInfo()
 
 def printUserInfo():
     userInfo = lingo.get_user_info()
-    for key, value in userInfo.items() :
-        print(key)
+    print("Name:", userInfo["fullname"])
+    print("Id:", userInfo["id"])
+    print("Languages:", userInfo["learning_language_string"])
+    print("Try out the 'help' command now!")
+    waitForCommand()
+
+def waitForCommand():
+    command = input(">>> ")
+    handle(command)
+    waitForCommand()
+    # or use while true?
+
+def handle(command):
+    print("Command", command, "called.")
+    if command == "help":
+        print("Try one of the following commands: words, exit")
+    elif command == "words":
+        words = lingo.get_known_words('es')
+        for (index, word) in enumerate(words):
+            print(index + 1, "\t", word)
+    elif command == "exit":
+        sys.exit(0)
 
 welcome()
