8cbefaae49ccf1e0d14669358e4c7565757d2fa8
Jarvis Wu <z235wu@edu.uwaterloo.ca>
2019-07-17 18:15:54 -0700

    Added translation

--- duo.py
+++ duo.py
@@ -10,6 +10,7 @@ import getpass
 lingo = None
 username = ""
 password = ""
+currentLanguage = ""
 
 def welcome():
     print("Welcome to Lingo, the Duolingo CLI app.")
@@ -36,15 +37,16 @@ def askLogin():
 
 def login():
     global lingo
+    global currentLanguage
     lingo = duolingo.Duolingo(username, password=password)
+    currentLanguage = lingo.get_languages(abbreviations=True)[0]
     print(colored("Login successful!", 'green', attrs = ['bold']))
     printUserInfo()
 
 def printUserInfo():
     userInfo = lingo.get_user_info()
-    print("Name:", userInfo["fullname"])
-    print("Id:", userInfo["id"])
-    print("Languages:", userInfo["learning_language_string"])
+    print("Name:", colored(userInfo["fullname"], 'red', attrs = ['bold']), end=" ")
+    print("Languages:", colored(userInfo["learning_language_string"], 'red', attrs = ['bold']))
     print("Try out the 'help' command now!")
     waitForCommand()
 
@@ -54,34 +56,45 @@ def waitForCommand():
     waitForCommand()
 
 def handle(command):
-    print("Command", command, "called.")
-    if command == "help":
+    mainCommand = command.split()[0]
+    # TDDO: how to parse parameters?
+    if mainCommand == "help":
         print("Try one of the following commands: words, related, vocabulary, exit")
-    elif command == "version":
+    elif mainCommand == "version":
         print("Lingo v0.1 2019")
-    elif command == "words":
+    elif mainCommand == "words":
         words = lingo.get_known_words('es')
         for (index, word) in enumerate(words):
             print(index + 1, "\t", word, "\t", "")
-    elif command == "exit":
+    elif mainCommand == "exit":
+        print("Quitting Lingo. See ya!")
         sys.exit(0)
-    elif command.split()[0] == "related":
+    elif mainCommand == "related":
         originalWord = command.split()[1]
         relatedWords = lingo.get_related_words(originalWord)
         printWordList(relatedWords)
-    elif command == "vocabulary":
+    elif mainCommand == "vocabulary":
         words = lingo.get_vocabulary()["vocab_overview"]
         printWordList(words)
+    elif mainCommand == "translate":
+        originalWord = command.split()[1]
+        translations = lingo.get_translations([originalWord], source="en", target="es")[originalWord]
+        for translation in translations:
+            print(translation)
+    else:
+        print("Unrecognized command.")
 
 def printWordList(words):
     for (index, word) in enumerate(words):
-        print('{}\t{:<25s}'.format(index + 1, word["word_string"]), end="")
+        coloredIndex = colored(str(index + 1).zfill(5), 'magenta', attrs = ['bold'])
+        bar = ""
         numOfBars = round(word["strength"] * 10)
         if numOfBars <= 4:
-            print(colored(numOfBars * "*", 'red', attrs = ['bold']))
+            bar = colored(numOfBars * "█", 'red')
         elif numOfBars <= 7:
-            print(colored(numOfBars * "*", 'yellow', attrs = ['bold']))
+            bar = colored(numOfBars * "█", 'yellow')
         else:
-            print(colored(numOfBars * "*", 'green', attrs = ['bold']))
+            bar = colored(numOfBars * "█", 'green')
+        print('{:<19}{:<20s}{}'.format(coloredIndex, bar, word["word_string"]))
 
 welcome()
